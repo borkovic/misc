86339c8cf72bb86aaa0cb1e37c1cfdbe2515b670
Drazen Borkovic <borkovic@nomail>
2016-09-24 11:46:21 -0700

    Instead of 'typedef struct{int sa_line_5__msg:0}line_5__msg' use 'struct line_5__msg{int sa_line_5__msg:0}'
    This restricts the declared identifier to 'struct tag' namespace in C. In
    C++ that does not make a difference since struct tag namespace is not separate.

--- src/static_assert/static_assert.h
+++ src/static_assert/static_assert.h
@@ -10,3 +10,6 @@
 #define STATIC_ASSERT(cond,msg) \
     typedef struct { int CATSTR(CATSTR(static_assert_line_,__LINE__), CATSTR(__,msg)) : ! !(cond); } CATSTR(static_assert_failed_line_,__LINE__)
 
+#define STATIC_ASSERT_B(cond,msg) \
+    struct CATSTR(static_assert_failed_line_,__LINE__) { int CATSTR(CATSTR(static_assert_line_,__LINE__), CATSTR(__,msg)) : ! !(cond); } 
+
--- src/static_assert/use_assert.c
+++ src/static_assert/use_assert.c
@@ -9,13 +9,14 @@ enum { X = 3, Y = 4};
 #define Yval 5
 #define Ycond Y_must_be_5
 
-STATIC_ASSERT(X==Xval, Xcond);
-
 /*
-STATIC_ASSERT(Y==Yval, Ycond);
 */
+STATIC_ASSERT(X==Xval, Xcond);
+STATIC_ASSERT(Y==Yval, Ycond);
 
 STATIC_ASSERT_A(X==Xval, Xcond);
-
 STATIC_ASSERT_A(Y==Yval, Ycond);
+
+STATIC_ASSERT_B(X==Xval, Xcond);
+STATIC_ASSERT_B(Y==Yval, Ycond);
 
