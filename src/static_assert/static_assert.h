074ac85352bcd7329eb6351126a699fcebebf719
Drazen Borkovic <borkovic@nomail>
2016-09-24 11:34:43 -0700

    Two versions of static_assert in C. One with array[-1], another with struct { bitfield : 0; }

---
+++ src/static_assert/static_assert.h
@@ -0,0 +1,12 @@
+/*****************************************************************************/
+#define CATSTR2(pre,post) pre##post
+#define CATSTR(pre,post) CATSTR2(pre,post)
+
+/*****************************************************************************/
+#define STATIC_ASSERT_A(COND,MSG) typedef char \
+    CATSTR(CATSTR(static_assert_line_,__LINE__),CATSTR(__,MSG))[2*(! !(COND))-1]
+
+/*****************************************************************************/
+#define STATIC_ASSERT(cond,msg) \
+    typedef struct { int CATSTR(CATSTR(static_assert_line_,__LINE__), CATSTR(__,msg)) : ! !(cond); } CATSTR(static_assert_failed_line_,__LINE__)
+
---
+++ src/static_assert/use_assert.c
@@ -0,0 +1,21 @@
+
+#include "static_assert.h"
+
+enum { X = 3, Y = 4};
+
+#define Xval 3
+#define Xcond X_must_be_3
+
+#define Yval 5
+#define Ycond Y_must_be_5
+
+STATIC_ASSERT(X==Xval, Xcond);
+
+/*
+STATIC_ASSERT(Y==Yval, Ycond);
+*/
+
+STATIC_ASSERT_A(X==Xval, Xcond);
+
+STATIC_ASSERT_A(Y==Yval, Ycond);
+
