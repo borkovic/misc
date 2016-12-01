287f60bf3d499c95808e8e8887586a983f9eef0a
Drazen Borkovic <borkovic@nomail>
2016-12-01 10:36:37 -0800

    Print tokens as enums rather than defines

--- byacc/output.c
+++ byacc/output.c
@@ -1159,6 +1159,8 @@ end_defines_file(void)
 #define end_defines_file()	/* nothing */
 #endif
 
+#define ENUM_TOKEN 1
+
 static void
 output_defines(FILE * fp)
 {
@@ -1167,10 +1169,22 @@ output_defines(FILE * fp)
 
     for (i = 2; i < ntokens; ++i)
     {
+#if ENUM_TOKEN
+	if (2 == i) {
+	    if (fp == code_file) outline += 3;
+	    fprintf(fp, "#ifndef YYTOKEN_IS_DECLARED\n");
+	    fprintf(fp, "#define YYTOKEN_IS_DECLARED 1\n");
+	    fprintf(fp, "typedef enum %s_token {\n", symbol_prefix);
+	}
+#endif
 	s = symbol_name[i];
 	if (is_C_identifier(s) && (!sflag || *s != '"'))
 	{
+#if ENUM_TOKEN
+	    fprintf(fp, "  ");
+#else
 	    fprintf(fp, "#define ");
+#endif
 	    c = *s;
 	    if (c == '"')
 	    {
@@ -1189,8 +1203,19 @@ output_defines(FILE * fp)
 	    }
 	    if (fp == code_file)
 		++outline;
+#if ENUM_TOKEN
+	    fprintf(fp, " = %d,\n", symbol_value[i]);
+#else
 	    fprintf(fp, " %d\n", symbol_value[i]);
+#endif
 	}
+#if ENUM_TOKEN
+	if (i + 1 == ntokens) {
+	    if (fp == code_file) outline += 2;
+	    fprintf(fp, "} %s_token;\n", symbol_prefix);
+	    fprintf(fp, "#endif /* !YYTOKEN_IS_DECLARED */\n");
+	}
+#endif
     }
 
     if (fp == code_file)
