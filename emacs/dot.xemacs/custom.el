9b2e2d88936d407ba978bae9a3d2fd8ec0dd8e50
Drazen Borkovic <borkovic@nomail>
2017-01-03 09:01:52 -0800

    Init files for xemacs

---
+++ emacs/dot.xemacs/custom.el
@@ -0,0 +1,6 @@
+(set-face-background 'default  "DarkSlateGray")
+(set-face-foreground 'default  "white")
+(custom-set-variables)
+(custom-set-faces
+   '(default ((t (:foreground "white" :background "DarkSlateGray"))) t)) 
+
