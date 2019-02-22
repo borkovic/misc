c7de2430af1cfa2943c2816b16f01287117d3915
Drazen Borkovic <dborkov@amazon.com>
2019-02-21 19:41:05 -0800

    Split heapsort from main
    Signed-off-by: Drazen Borkovic <dborkov@amazon.com>

--- go/hs1.go
+++ go/hs1.go
@@ -3,101 +3,6 @@ package main
 import "fmt"
 import "math/rand"
 
-type Index int
-type Value int32
-type Vec []Value
-
-func Len(v Vec) Index {
-    return Index(len(v))
-}
-
-/*
-            0
-         1     2
-        3 4   5 6
-    children->parent
-    1,2     ->0
-    3,4     ->1
-    5,6     ->2
-* Parent, Left and right child in array based heap
-*/
-func parent(k Index) Index {
-    if k <= 0 {
-        panic("Negative index in parent")
-    }
-    return (k - 1) / 2
-}
-
-func lChild(k Index) Index {
-    return 2*k + 1
-}
-
-func rChild(k Index) Index {
-    return 2*k + 2
-}
-
-type CmpFunc func(l Value, r Value) int
-
-/* Move element k towards root if it small
- */
-func toRoot(v Vec, k Index, cmp CmpFunc) {
-    val := v[k]
-    for k > 0 {
-        p := parent(k)
-        //fmt.Println("TR: ", v)
-        //fmt.Println("TR: k=",k, "v[k]=",v[k], "p=",p, "v[p]=",v[p])
-        if cmp(v[p], val) <= 0 {
-            break
-        }
-        v[k] = v[p]
-        k = p
-    }
-    v[k] = val
-}
-
-/* Move element k toward leaves if it is large
- */
-func toLeaves(v Vec, k Index, last Index, cmp CmpFunc) {
-    val := v[k]
-    for leftChild := lChild(k); leftChild <= last; leftChild = lChild(k) { // k has at least one child
-        smallChild := leftChild
-        rightChild := leftChild + 1
-        if rightChild <= last && cmp(v[rightChild], v[smallChild]) < 0 {
-            smallChild = rightChild
-        }
-        //fmt.Println(v, k, v[k], smallChild, v[smallChild])
-        if cmp(v[smallChild], val) >= 0 {
-            break
-        }
-        v[k] = v[smallChild]
-        k = smallChild
-    }
-    v[k] = val
-}
-
-/* Make heap with elem[0] being root, smallest in heap
- */
-func heapify(v Vec, cmp CmpFunc) {
-    last := Len(v) - 1
-    for k := parent(last); k >= 0; k-- {
-        toLeaves(v, k, last, cmp)
-    }
-}
-
-/* Heapsort in descending order
- */
-func heapsort(v Vec, cmp CmpFunc) {
-    // make heap in linear time
-    //fmt.Println("A"); prHeap(v[:], 0, "")
-    //fmt.Println(v)
-    heapify(v, cmp)
-    //fmt.Println("B"); prHeap(v[:], 0, "")
-    last := Len(v) - 1
-    for k := last; k >= 1; k-- {
-        v[0], v[k] = v[k], v[0]
-        toLeaves(v, 0, k-1, cmp)
-    }
-}
 
 func checkSorted(v Vec, cmp CmpFunc) {
     last := Len(v) - 1
@@ -118,7 +23,7 @@ func checkSorted(v Vec, cmp CmpFunc) {
 /*
  * Compare Less Than
  */
-func CmpLT(l Value, r Value) int {
+func cmpLT(l Value, r Value) int {
     if l < r {
         return -1
     } else if l > r {
@@ -131,8 +36,8 @@ func CmpLT(l Value, r Value) int {
 /*
  * Compare Greater Than
  */
-func CmpGT(l Value, r Value) int {
-    return CmpLT(r, l)
+func cmpGT(l Value, r Value) int {
+    return cmpLT(r, l)
 }
 
 func prHeap(v Vec, k Index, ident string) {
@@ -157,7 +62,9 @@ func main() {
     for i := 0; i < len(v); i++ {
         v[i] = Value(rand.Int31n(N))
     }
-    cmp := CmpGT
-    heapsort(v[:], cmp)
+    cmp := cmpGT
+    Heapsort(v[:], cmp)
     checkSorted(v[:], cmp)
 }
+
+
---
+++ go/hs1a.go
@@ -0,0 +1,102 @@
+package main
+
+
+type Index int
+type Value int32
+type Vec []Value
+
+func Len(v Vec) Index {
+    return Index(len(v))
+}
+
+type CmpFunc func(l Value, r Value) int
+
+/*
+            0
+         1     2
+        3 4   5 6
+
+    children -> parent
+       1,2   ->   0
+       3,4   ->   1
+       5,6   ->   2
+* Parent, Left and right child in array based heap
+*/
+func parent(k Index) Index {
+    if k <= 0 {
+        panic("Negative index in parent")
+    }
+    return (k - 1) / 2
+}
+
+func lChild(k Index) Index {
+    return 2*k + 1
+}
+
+func rChild(k Index) Index {
+    return 2*k + 2
+}
+
+
+/* Move element k towards root if it small
+ */
+func toRoot(v Vec, k Index, cmp CmpFunc) {
+    val := v[k]
+    for k > 0 {
+        p := parent(k)
+        //fmt.Println("TR: ", v)
+        //fmt.Println("TR: k=",k, "v[k]=",v[k], "p=",p, "v[p]=",v[p])
+        if cmp(v[p], val) <= 0 {
+            break
+        }
+        v[k] = v[p]
+        k = p
+    }
+    v[k] = val
+}
+
+/* Move element k toward leaves if it is large
+ */
+func toLeaves(v Vec, k Index, last Index, cmp CmpFunc) {
+    val := v[k]
+    for leftChild := lChild(k); leftChild <= last; leftChild = lChild(k) { // k has at least one child
+        smallChild := leftChild
+        rightChild := leftChild + 1
+        if rightChild <= last && cmp(v[rightChild], v[smallChild]) < 0 {
+            smallChild = rightChild
+        }
+        //fmt.Println(v, k, v[k], smallChild, v[smallChild])
+        if cmp(v[smallChild], val) >= 0 {
+            break
+        }
+        v[k] = v[smallChild]
+        k = smallChild
+    }
+    v[k] = val
+}
+
+/* Make heap with elem[0] being root, smallest in heap
+ */
+func heapify(v Vec, cmp CmpFunc) {
+    last := Len(v) - 1
+    for k := parent(last); k >= 0; k-- {
+        toLeaves(v, k, last, cmp)
+    }
+}
+
+/* Heapsort in descending order
+ */
+func Heapsort(v Vec, cmp CmpFunc) {
+    // make heap in linear time
+    //fmt.Println("A"); prHeap(v[:], 0, "")
+    //fmt.Println(v)
+    heapify(v, cmp)
+    //fmt.Println("B"); prHeap(v[:], 0, "")
+    last := Len(v) - 1
+    for k := last; k >= 1; k-- {
+        v[0], v[k] = v[k], v[0]
+        toLeaves(v, 0, k-1, cmp)
+    }
+}
+
+
