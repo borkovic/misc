29f658db240bcd3610496305f7f125d1499f45f3
Drazen Borkovic <borkovic@nomail>
2019-04-24 22:10:49 -0700

    Fmt golang. Noexpand tab for golang in vimrc

--- go/src/self-reference/sr.go
+++ go/src/self-reference/sr.go
@@ -6,39 +6,36 @@ import "fmt"
 
 //**************************************************************
 type Foo struct {
-    verbosity int
+	verbosity int
 }
 
 type option func(f *Foo) option
 
-
 //**************************************************************
 func (f *Foo) Option(opts ...option) (previous option) {
-    for _, opt := range opts {
-        previous = opt(f)
-    }
-    return previous
+	for _, opt := range opts {
+		previous = opt(f)
+	}
+	return previous
 }
 
 //**************************************************************
 // Verbosity sets Foo's verbosity level to v.
 func Verbosity(v int) option {
-    return func(f *Foo) option {
-        previous := f.verbosity
-        fmt.Println("Setting verbosity to", v, ", Old verbosity", f.verbosity)
-        f.verbosity = v
-        return Verbosity(previous)
-    }
+	return func(f *Foo) option {
+		previous := f.verbosity
+		fmt.Println("Setting verbosity to", v, ", Old verbosity", f.verbosity)
+		f.verbosity = v
+		return Verbosity(previous)
+	}
 }
-
 
 //**************************************************************
 func main() {
-    var f Foo 
-    f.verbosity = 14
+	var f Foo
+	f.verbosity = 14
 
-    prevVerbosity := f.Option(Verbosity(3))
-    //DoSomeDebugging()
-    f.Option(prevVerbosity)
+	prevVerbosity := f.Option(Verbosity(3))
+	//DoSomeDebugging()
+	f.Option(prevVerbosity)
 }
-
