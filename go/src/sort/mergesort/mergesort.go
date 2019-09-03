edaa35a9ab131c63149f19fd52701f72bbb6d5d3
Drazen Borkovic <borkovic@nomail>
2019-09-02 20:54:54 -0700

    Sequential mergesort works

--- go/src/sort/mergesort/mergesort.go
+++ go/src/sort/mergesort/mergesort.go
@@ -1,5 +1,6 @@
 //***************************************************************************
 package mergesort
+
 import (
 	"fmt"
 )
@@ -11,23 +12,19 @@ import (
 type ValType = utils.ValType
 type IndexType = utils.IndexType
 
-
 //***************************************************************************
-// Recursive quicksort.
-// 1. Partition into two parts. Note that pivot_high==pivot_low.
-// 2. Recurse only on the shorter part to limit stack depth to log(N)
-// 3. Continue looping with the longer part
-// 4. Sorted array will and in v2
+// Recursive mergesort`.
+// v2 is aux array used for merging
 //***************************************************************************
 func mergesort(v, v2 []ValType) {
-	const debug bool = true
+	const debug bool = false
 
 	Begin := IndexType(0)
 	End := IndexType(len(v))
-	if Begin + 1 >= End {
+	if End-Begin <= 1 {
 		return
 	}
-	if Begin + 2 == End {
+	if End-Begin <= 2 {
 		if v[0] > v[1] {
 			v[0], v[1] = v[1], v[0]
 		}
@@ -38,41 +35,39 @@ func mergesort(v, v2 []ValType) {
 	mergesort(v[Begin:mid], v2[Begin:mid])
 	if debug {
 		fmt.Println("Bot:", Begin, ",", mid)
-		fmt.Println(v[Begin:mid])
+		fmt.Println("> ", v[Begin:mid])
 	}
 	mergesort(v[mid:End], v2[mid:End])
 	if debug {
 		fmt.Println("Top:", mid, ",", End)
-		fmt.Println("> ",v[mid:End])
+		fmt.Println("> ", v[mid:End])
 	}
 
-	i := IndexType(Begin)
-	j := IndexType(mid)
-	x := IndexType(Begin)
+	i := Begin
+	j := mid
+	x := Begin
 
 	for i < mid && j < End {
-		var s IndexType
-		if v2[i] <= v2[j] {
-			s = i
+		if v[i] <= v[j] {
+			v2[x] = v[i]
 			i++
 		} else {
-			s = j
+			v2[x] = v[j]
 			j++
 		}
-		v[x] = v2[s]
 		x++
 	}
 	for i < mid {
-		v[x] = v2[i]
+		v2[x] = v[i]
 		x++
 		i++
 	}
 	for j < End {
-		v[x] = v2[j]
+		v2[x] = v[j]
 		x++
 		j++
 	}
-	//copy(v2, v)
+	copy(v, v2)
 }
 
 //***************************************************************************
