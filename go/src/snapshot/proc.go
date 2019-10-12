d8017f90fd839df57093c7774e0af55ad7bb1992
Drazen Borkovic <borkovic@nomail>
2019-10-12 00:55:51 -0700

    Print under Debug

--- go/src/snapshot/proc.go
+++ go/src/snapshot/proc.go
@@ -9,6 +9,11 @@ import (
 	"time"
 )
 
+const (
+	//Debug bool = true
+	Debug bool = false
+)
+
 /*************************************************************
 *************************************************************/
 func (proc *Proc) SLEEP(nn int) {
@@ -25,15 +30,15 @@ func (proc *Proc) SLEEP(nn int) {
  * Collects sums from the children
 *************************************************************/
 func (proc *Proc) runRoot(neighbors NeighborChans) Data {
-	numNeighbors := len(neighbors)
+	//numNeighbors := len(neighbors)
 	for _, n := range neighbors {
 		proc.SLEEP(88)
 		n.Out <- (-proc.m_MyVal)
 	}
 	proc.SLEEP(4)
 	var sum Data = (proc.m_MyVal)
-	for i := 0; i < numNeighbors; i++ {
-		x := <-neighbors[i].In
+	for _, n := range neighbors {
+		x := <-n.In
 		proc.SLEEP(8)
 		if x > 0 {
 			sum += x
@@ -51,40 +56,45 @@ func (proc *Proc) runRoot(neighbors NeighborChans) Data {
  * 5. send sum to parent
 *************************************************************/
 func (proc *Proc) runChild(neighbors NeighborChans) {
-	numNeighbors := len(neighbors)
+	//numNeighbors := len(neighbors)
 	var parIdx int = -1
+	var parVal Data
 
 	proc.SLEEP(4)
 	// 1. read from parent In tree
 	select {
-	case <-neighbors[0].In:
+	case parVal = <-neighbors[0].In:
 		parIdx = 0
-	case <-neighbors[1].In:
+	case parVal = <-neighbors[1].In:
 		parIdx = 1
-	case <-neighbors[2].In:
+	case parVal = <-neighbors[2].In:
 		parIdx = 2
+	}
+	if Debug {
+		fmt.Println("Child with val ", proc.m_MyVal, ", par idx ", parIdx,
+					", par value ", parVal)
 	}
 
 	// 2. send to all but parent (children and siblings)
-	for i := 0; i < numNeighbors; i++ {
+	for i, n := range neighbors {
 		if i == parIdx {
 			proc.SLEEP(4)
 			continue
 		}
 		proc.SLEEP(4)
-		neighbors[i].Out <- (-proc.m_MyVal)
+		n.Out <- (-proc.m_MyVal)
 	}
 
 	// 3. receive from children and siblings
 	// from children positive, from siblings negative
 	// 4. sum from children
 	sum := (proc.m_MyVal)
-	for i := 0; i < numNeighbors; i++ {
+	for i, n := range neighbors {
 		if i == parIdx {
 			continue
 		}
 		proc.SLEEP(4)
-		v := <-neighbors[i].In
+		v := <-n.In
 		if v > 0 { // this is child, siblings send negative
 			sum += v
 		}
@@ -108,8 +118,17 @@ func (proc *Proc) runChild2(neighbors NeighborChans) {
 	proc.SLEEP(2)
 	// 1. read from parent In tree
 
-	/***********************************************************/
-	/*
+	/***********************************************************
+	 * select {
+	 * case <-neighbors[0].In:
+	 *     parIdx = 0
+	 * case <-neighbors[1].In:
+	 *     parIdx = 1
+	 * case <-neighbors[2].In:
+	 *     parIdx = 2
+	 * }
+	 *
+	 *
 	 * package reflect
 	 * type SelectCase struct {
 	 *     Dir  SelectDir // direction of case
@@ -127,7 +146,8 @@ func (proc *Proc) runChild2(neighbors NeighborChans) {
 	 * )
 	 *
 	 * func Select(cases []SelectCase) (chosen int, recv Value, recvOK bool)
-	 */
+	 *
+	***********************************************************/
 	cases := make([]reflect.SelectCase, numNeighbors)
 	for i := 0; i < numNeighbors; i++ {
 		cases[i].Dir = reflect.SelectRecv
@@ -137,18 +157,12 @@ func (proc *Proc) runChild2(neighbors NeighborChans) {
 	var parIdx int = -1
 	var parVal reflect.Value
 	parIdx, parVal, _ = reflect.Select(cases)
-	fmt.Println("Child with val ", proc.m_MyVal, ", par idx ", parIdx, ", par value ", parVal)
 
-	/*
-		select {
-		case <-neighbors[0].In:
-			parIdx = 0
-		case <-neighbors[1].In:
-			parIdx = 1
-		case <-neighbors[2].In:
-			parIdx = 2
-		}
-	*/
+	if Debug {
+		fmt.Println("Child with val ", proc.m_MyVal, ", par idx ", parIdx,
+					", par value ", parVal)
+	}
+
 	/***********************************************************/
 
 	// 2. send to all but parent (children and siblings)
@@ -177,8 +191,10 @@ func (proc *Proc) runChild2(neighbors NeighborChans) {
 	}
 
 	// 5. send sum to parent
-	fmt.Println("Child back to parent: my val ", proc.m_MyVal,
-		", sum ", sum, ", par val ", parVal)
+	if Debug {
+		fmt.Println("Child back to parent: my val ", proc.m_MyVal,
+					", sum ", sum, ", par val ", parVal)
+	}
 	neighbors[parIdx].Out <- sum
 }
 
@@ -192,12 +208,16 @@ func (proc *Proc) Run(topChan *VertChanPair, neighbors NeighborChans) {
 
 	proc.m_MyVal = <-topChan.In
 	if proc.m_MyVal < 0 { // root
-		fmt.Println("Run root, my val ", proc.m_MyVal)
+		if Debug {
+			fmt.Println("Run root, my val ", proc.m_MyVal)
+		}
 		proc.m_MyVal = -proc.m_MyVal
 		sum := proc.runRoot(neighbors)
 		topChan.Out <- sum
 	} else { // slave
-		fmt.Println("Run child, my val ", proc.m_MyVal)
+		if Debug {
+			fmt.Println("Run child, my val ", proc.m_MyVal)
+		}
 		proc.runChild2(neighbors)
 	}
 }
