b0c72058388526746380c2529370f632410e536c
Drazen Borkovic <borkovic@github.com>
2019-10-13 14:26:43 -0700

    Separate child and root to separate files
    Signed-off-by: Drazen Borkovic <borkovic@github.com>

---
+++ go/src/snapshot/root.go
@@ -0,0 +1,25 @@
+package snapshot
+
+
+/*************************************************************
+ * This process is root.
+ * It sends its neg. value to all children.
+ * Collects sums from the children
+*************************************************************/
+func (proc *Proc) runRoot(neighbors NeighborChans) Data {
+	//numNeighbors := len(neighbors)
+	for _, n := range neighbors {
+		proc.SLEEP(88)
+		n.Out <- (-proc.m_MyVal)
+	}
+	proc.SLEEP(4)
+	var sum Data = (proc.m_MyVal)
+	for _, n := range neighbors {
+		x := <-n.In
+		proc.SLEEP(8)
+		if x > 0 {
+			sum += x
+		}
+	}
+	return Data(sum)
+}
