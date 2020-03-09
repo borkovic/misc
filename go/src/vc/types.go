2a93001846729e284d98c33041b12891ca1b3b90
Drazen Borkovic <borkovic@nomail>
2020-03-08 18:35:23 -0700

    Split external interfaces into ext_ifc.go

--- go/src/vc/types.go
+++ go/src/vc/types.go
@@ -1,25 +1,8 @@
 package vc
 
 //**********************************************
-type EngineIdx int16
 type OpIndexOnEng int32
 type OpIndexExternal int32
-
-//**********************************************
-type ExtOp interface {
-	NumPreds() int16
-	NumSuccs() int16
-	Pred(n int16) ExtOp
-	Succ(n int16) ExtOp
-	EngineIdx() EngineIdx
-}
-
-//**********************************************
-type ExtGraph interface {
-	NumOps() int32
-	Op(n int32) ExtOp
-}
-
 
 //**********************************************
 type Op struct {
