ad1a42eb35ac3cb79f128c14eac4ab843f84951d
Drazen Borkovic <dborkov@amazon.com>
2020-05-02 15:05:07 -0700

    Add (-1) as end marker
    Signed-off-by: Drazen Borkovic <dborkov@amazon.com>

--- go/src/prime/prime.go
+++ go/src/prime/prime.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+var MAX int = 1000
+
 func generate_integers() <-chan int {
 	c := make(chan int)
 
@@ -9,7 +11,11 @@ func generate_integers() <-chan int {
 	go func() {
 		for i := 2; ; i++ {
 			ch <- i
+			if i > MAX {
+				break
+			}
 		}
+		ch <- -1  // end marker
 	}()
 
 	return c
@@ -21,7 +27,12 @@ func filter_multiples(in <-chan int, prime int) <-chan int {
 	var out chan<- int = o
 	go func() {
 		for {
-			if i := <-in; i%prime != 0 {
+			i := <-in
+			if i < 0 {
+				out <- i
+				break
+			}
+			if i%prime != 0 {
 				out <- i
 			}
 		}
@@ -39,6 +50,9 @@ func sieve() <-chan int {
 		for {
 			prime := <-ch
 			out <- prime
+			if prime < 0 {
+				break
+			}
 			ch = filter_multiples(ch, prime)
 		}
 	}()
@@ -51,11 +65,16 @@ func main() {
 	N := 10
 	i := 0
 	for {
-		fmt.Print(<-primes, " ")
+		p := <-primes
+		if p < 0 {
+			break
+		}
+		fmt.Print(p, " ")
 		i++
 		if i == N {
 			i = 0
 			fmt.Println("")
 		}
 	}
+	fmt.Println("")
 }
