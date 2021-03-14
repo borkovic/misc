01609b1b599a1994898e81a9070caffadb011943
Drazen Borkovic <borkovic@github.com>
2021-03-14 16:32:13 -0700

    blob.go
    Signed-off-by: Drazen Borkovic <borkovic@github.com>

---
+++ go/src/gitlet/blob.go
@@ -0,0 +1,29 @@
+package gitlet
+
+//import "io"
+//import "fmt"
+//import "encoding/hex"
+import "crypto/sha1"
+import "encoding/hex"
+
+type ShaId struct {
+	Data [sha1.Size]byte
+}
+
+type Blob struct {
+	FileId ShaId
+}
+
+type Commit struct {
+	Files map[string]Blob // file-name to sha
+	ParentCommit ShaId
+}
+
+
+func (sha *ShaId) Bytes() []byte {
+	return sha.Data[:]
+}
+
+func (sha *ShaId) Name() string {
+	return hex.EncodeToString(sha.Bytes())
+}
